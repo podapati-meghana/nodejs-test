133cdb01939e0c667709bd818e645e4866caf335
podapati-meghana <101330900+podapati-meghana@users.noreply.github.com>
2025-03-15 15:10:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Nodejs and say hello again');
+  res.end('Hello World from Nodejs');
 });
  
 server.listen(port, () => {
